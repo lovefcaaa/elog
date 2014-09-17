b957434dd10477415d8b29a933b8dccbff00fc0e
lovefcaaa <lovefcaaa@126.com>
2014-09-17 16:42:52 +0800

    Update install.php

--- install.php
+++ install.php
@@ -1,4 +1,17 @@
 <?php
+/*
+* PHP Enterprise-level log
+*
+* PHP Enterprise-level log is distributed under GPL 2
+* Copyright (C) 2014 lovefcaaa <https://github.com/lovefcaaa>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or any later version.
+*/
+
+<?php
 /**
  * Php_Log install
  * @author: lovefcaaa
@@ -7,5 +20,5 @@ error_reporting(E_ALL);
 require_once('./file.class.php');
 require_once('./mysql.class.php');
 
-Php_Log_Logger_File::instance()->install();
-Php_Log_Logger_Mysql::instance()->install();
+elog_Logger_File::instance()->install();
+elog_Logger_Mysql::instance()->install();
